61129803967f2071447a2dca93102422d6ee7eb0
vineet574 <113534980+vineet574@users.noreply.github.com>
2024-10-30 03:55:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,40 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const helmet = require('helmet');
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('public'));
+// Apply various security headers
 app.use(helmet({
   contentSecurityPolicy: false,
+  xssFilter: true,
+  noSniff: true,
+  hidePoweredBy: { setTo: 'PHP 7.4.3' },
+}));
+
+// Prevent MIME sniffing
+app.use((req, res, next) => {
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  next();
+});
+
+// Disable client-side caching
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  next();
+});
+
+// Serve static files from 'public', set cache control headers
+app.use(express.static('public', {
+  setHeaders: function (res, path) {
+    res.setHeader('Cache-Control', 'no-cache');
+  }
 }));
 
 io.on('connection', (socket) => {
@@ -26,3 +52,4 @@ app.get('/', (req, res) => {
 server.listen(3000, () => {
   console.log('Listening on *:3000');
 });
+
